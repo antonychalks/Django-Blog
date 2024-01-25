994d92d0235cbf8629baa02e9ac12845f1e2f582
antonychalks <>
2024-01-25 19:20:35 +0000

    Add secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vvxtfk4yg1l(13l#da9qi*zmi&kdes$i=d_3j*r+t^)#(!!6+j'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# 'django-insecure-vvxtfk4yg1l(13l#da9qi*zmi&kdes$i=d_3j*r+t^)#(!!6+j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
