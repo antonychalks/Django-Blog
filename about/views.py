4281a39039d6ca68eb175bb3b138c3322c242f3f
antonychalks <>
2024-02-07 16:33:39 +0000

    add functionality to send a collaboration request

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 
 # Create your views here.
@@ -12,10 +14,23 @@ def about_me(request):
     :template:`about/about.html`
     """
 
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Collaboration request received! I endeavour to respond within 2 working days."
+            )
+
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form
+        },
     )
--- blog/views.py
+++ blog/views.py
@@ -33,6 +33,7 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
 
     if request.method == "POST":
+        print("recieved a post request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -46,6 +47,7 @@ def post_detail(request, slug):
 
     comment_form = CommentForm()
 
+    print("Starting template render.")
     return render(
         request,
         "blog/post_detail.html",
