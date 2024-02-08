44847566b2f7ddecc6810aa1d2bab9b38438df01
antonychalks <>
2024-02-08 20:21:58 +0000

    Added doc strings and passed through validators

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,9 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
+    """
+    Stores a single blog post entry related to :model:`auth.User`.
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -24,22 +27,24 @@ class Post(models.Model):
         ordering = ["-created_on"]
 
     def __str__(self):
-        return f"{self.title}| written by {self.author}"
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
-    )
+    """
+    Stores a single comment entry related to :model:`auth.User`
+    and :model:`blog.Post`.
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter"
-    )
-    comment = models.TextField()
-    approved = models.BooleanField(default=False)
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.comment} by {self.author}"
+        return f"Comment {self.body} by {self.author}"
